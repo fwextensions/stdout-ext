337423159b6a4d8ef03e06878f630124afe3422e
John Dunning <fw@johndunning.com>
2024-08-23 19:15:52 -0700

    Add webpack and babel
    Log messages to the extensions console as well.
    Send a response to each message so the sender service worker can unload faster.
    Make console fit entire window.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ export default function App()
 {
 	const [logs, setLogs] = useState([]);
 
-	const handleMessage = useCallback((message, sender) => {
+	const handleMessage = useCallback((message, sender, sendResponse) => {
 		const { method, data } = message;
 
 		if (data) {
+			console[method].apply(console, data);
 			setLogs((currentLogs) => [...currentLogs, { method, data }]);
 		}
+
+			// if we don't call sendResponse(), Chrome will keep the calling service worker
+			// alive waiting for a response, up to 5 minutes.  sending the response here
+			// lets the service worker get terminated after 30 seconds.
+		sendResponse(null);
 	}, []);
 
 	useEffect(() => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-//import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-console.log("derp");
-createRoot(document.getElementById("root")).render(<App />);
-
-//createRoot(document.getElementById("root")).render(
-//	<StrictMode>
-//		<App />
-//	</StrictMode>,
-//);
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
